7afc81a3be22098c6d51420ee39b3af7da66abd3
JLucBlennie <jluc.blennie@gmail.com>
2026-02-24 17:27:27 +0100

    Gestion avec NGinx

--- web.ui/next.config.mjs
+++ web.ui/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Pas besoin de basePath, car NextJS est à la racine
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        // En production (VPS), proxy vers Quarkus sur le même serveur
+        destination: process.env.NODE_ENV === 'production'
+          ? 'http://api.blondy29.ovh/ctr/:path*'
+          : 'http://localhost:9090/ctr/:path*', // Pour le dev local, si Quarkus tourne aussi en local
+      },
+    ];
+  },
+};
 
 export default nextConfig;
